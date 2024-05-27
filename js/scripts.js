88b874547f99abf7e64a39d3591d4f569530ef96
YannisAd <yannaddi48@gmail.com>
2024-05-27 15:54:14 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -95,3 +95,28 @@ document.getElementById("language-toggle").addEventListener("change", toggleLang
             });
         }
     }
+
+
+
+
+ $('.gallery ul li a').click(function() {
+    var itemID = $(this).attr('href');
+    $('.gallery ul').addClass('item_open');
+    $(itemID).addClass('item_open');
+    return false;
+});
+$('.close').click(function() {
+    $('.port, .gallery ul').removeClass('item_open');
+    return false;
+});
+
+$(".gallery ul li a").click(function() {
+    $('html').animate({
+        scrollTop: parseInt($("#top").offset().top)
+    }, 200);
+});
+
+
+
+
+
---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ['index.html'], // Adaptez le chemin selon votre structure de fichiers
+    css: ['./css/styles.css'], // Chemin vers votre fichier CSS
+    output: './css/', // Répertoire de sortie pour le fichier CSS purgé
+  };
+  
