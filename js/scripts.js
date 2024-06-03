af8a88b0f40fcd9435aac946a068aa47a7bb32b0
YannisAd <yannaddi48@gmail.com>
2024-06-03 15:55:39 +0200

    responsive test

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -23,6 +13,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
+
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
@@ -30,9 +21,7 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
-
 
 $(document).ready(function() {
     $('.list-inline-item i').hover(
@@ -44,40 +33,39 @@ $(document).ready(function() {
             $(this).next('.description').remove();
         }
     );
-});
 
+    // Hide all open boxes
+    $(".open-box").css("display", "none");
 
-//hide all the open boxes
-$(".open-box").css("display", "none")
+    // Loop to associate the closed-box with its open-box
+    var i = 0;
+    var numberOfBoxes = $(".closed-box").length;
 
-//loop to associate the closed-box with its open-box
-var i = 0;
-var numberOfBoxes = $(".closed-box").length ;
+    while (i < numberOfBoxes) {
+        (function(i) {
+            // On clicking a closed box...
+            $(".closed-box").eq(i).click(function() {
+                // Close all the boxes that may already have been opened
+                $(".open-box").slideUp();
+                // If the open-box associated with the box we're clicking on is closed, open it
+                if ($(".open-box").eq(i).is(":hidden")) {
+                    $(".open-box").eq(i).slideDown("slow");
+                } else {
+                    // Otherwise, hide any open boxes
+                    $(".open-box").slideUp();
+                }
+            });
+        })(i);
+        i++;
+    }
 
-while (i <= numberOfBoxes) {
-  function iffe(i) {
-    //on clicking a closed box...
-    $(".closed-box").eq(i).click(function() {
-      //close all the boxes that may already have been opened
-      $(".open-box").slideUp();
-      //if the open-box associated with the box we're clicking on is closed, open it
-      if ($(".open-box").eq(i).is(":hidden")) {
-        $(".open-box").eq(i).slideDown("slow");
-        //otherwise, hide any open boxes
-      } else {
+    // When user clicks on close button, open boxes close
+    $(".close-button").click(function() {
         $(".open-box").slideUp();
-      }
     });
-  }
-  iffe(i);
-  i++;
-}
-//when user clicks on close button, open boxes close
-$(".close-button").click(function() {
-  $(".open-box").slideUp();
-});
 
-document.getElementById("language-toggle").addEventListener("change", toggleLanguage);
+    // Language toggle
+    document.getElementById("language-toggle").addEventListener("change", toggleLanguage);
 
     function toggleLanguage() {
         var elements = document.querySelectorAll("[data-french], [data-english]");
@@ -96,27 +84,22 @@ document.getElementById("language-toggle").addEventListener("change", toggleLang
         }
     }
 
+    // Gallery functionality
+    $('.gallery ul li a').click(function() {
+        var itemID = $(this).attr('href');
+        $('.gallery ul').addClass('item_open');
+        $(itemID).addClass('item_open');
+        return false;
+    });
 
+    $('.close').click(function() {
+        $('.port, .gallery ul').removeClass('item_open');
+        return false;
+    });
 
-
- $('.gallery ul li a').click(function() {
-    var itemID = $(this).attr('href');
-    $('.gallery ul').addClass('item_open');
-    $(itemID).addClass('item_open');
-    return false;
+    $(".gallery ul li a").click(function() {
+        $('html').animate({
+            scrollTop: parseInt($("#top").offset().top)
+        }, 200);
+    });
 });
-$('.close').click(function() {
-    $('.port, .gallery ul').removeClass('item_open');
-    return false;
-});
-
-$(".gallery ul li a").click(function() {
-    $('html').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 200);
-});
-
-
-
-
-
